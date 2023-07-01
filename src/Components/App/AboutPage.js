73ef2f3688a881ba0a5fe5998da8ec2fe7628f4d
DestinyJoyner <destinyjoyner@pursuit.org>
2023-07-01 09:54:00 -0400

    cleaned up about component

--- src/Components/App/AboutPage.js
+++ src/Components/App/AboutPage.js
@@ -1,19 +1,20 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid"
-import SkillsComponent from "../Job/SkillsComponent.js"
-import { BsLinkedin, BsGithub } from "react-icons/bs";
-import { GrMail } from "react-icons/gr"
-import { team } from "./AboutData";
-import DMphoto from "../../Assets/DMphoto.png";
-import SMphoto from "../../Assets/SMphoto.jpg";
-import DJphoto from "../../Assets/DJphoto.png"
+import { v4 as uuidv4 } from "uuid";
 import Header from "../Job/Header";
+import SkillsComponent from "../Job/SkillsComponent.js";
+import { team } from "./AboutData";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { GrMail } from "react-icons/gr";
 import "../App/AboutPage.css";
 
-function AboutPage(props) {
+function AboutPage() {
   const [profileCard, setProfileCard] = useState(team["6"]);
 
-  const iconArr = [<BsGithub />, <BsLinkedin />, <GrMail className="about-mail" />];
+  const iconArr = [
+    <BsGithub />,
+    <BsLinkedin />,
+    <GrMail className="about-mail" />,
+  ];
 
   function aboutCard(e) {
     if (profileCard.id === +e.target.id) {
@@ -30,33 +31,30 @@ function AboutPage(props) {
         <img
           id="1"
           className="devicon"
-          src={DJphoto}
+          src={team["1"].img}
           onClick={(event) => aboutCard(event)}
         ></img>
         <img
           id="2"
           className="devicon"
-          src={DMphoto}
+          src={team["2"].img}
           onClick={(event) => aboutCard(event)}
         ></img>
         <img
           id="3"
           className="devicon"
-          src={SMphoto}
+          src={team["3"].img}
           onClick={(event) => aboutCard(event)}
         ></img>
       </div>
-      {
-      profileCard.id === 6 ? 
-      (
+      {profileCard.id === 6 ? (
         <div className="emptyState grid-center">
           <h2>{profileCard.name}</h2>
           <img src={profileCard.img}></img>
           <p>{profileCard.bio}</p>
-          <hr/>
-          <SkillsComponent justList={true}
-          skillsArr={profileCard.links} />
-          <hr/>
+          <hr />
+          <SkillsComponent justList={true} skillsArr={profileCard.links} />
+          <hr />
         </div>
       ) : (
         <div className="indiv-card grid-center">
@@ -65,14 +63,11 @@ function AboutPage(props) {
           <img className="devicon2" src={profileCard.img}></img>
           <h3>{profileCard.role}</h3>
           <p>{profileCard.bio}</p>
-          <hr/>
+          <hr />
           <div className="dev-socials">
             {profileCard.links.length > 0 &&
               profileCard.links.map((el, i) => (
-                <a 
-                key={uuidv4()}
-                href={el} 
-                target="_blank">
+                <a key={uuidv4()} href={el} target="_blank">
                   {iconArr[i]}
                 </a>
               ))}
