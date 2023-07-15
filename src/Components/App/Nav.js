b326bf5dac1326c4f621ed3bf8e99e907d765a7a
DestinyJoyner <destinyjoyner@pursuit.org>
2023-07-15 01:39:24 -0400

    job show header syling altered

--- src/Components/App/Nav.js
+++ src/Components/App/Nav.js
@@ -1,23 +1,17 @@
-// import { useNavigate } from "react-router-dom";
 import { useContextProvider } from "../../Providers/Provider";
 import SlideNav from "./SlideNav";
 import Header from "../Job/Header.js"
-// import { RxHamburgerMenu } from "react-icons/rx";
-// import { GiHamburgerMenu } from "react-icons/gi"
 import hamburger from "../../Assets/blue-hamburger-menu.png"
 import "./Nav.css";
 
 export default function Nav() {
   const { navbarClick, appHeader } = useContextProvider();
-  // const navigate = useNavigate();
 
   return (
     <nav className="grid-center">
       <img src={hamburger} alt="hamburger-menu"
-      className="burger2" onClick={() => navbarClick()} />
-        {/* <RxHamburgerMenu className="burger" onClick={() => navbarClick()} /> */}
+      className="burger" onClick={() => navbarClick()} />
       <Header header={appHeader} />
-
       <SlideNav />
     </nav>
   );
