600ef436538e2cd4f27f786d0f73ee3f37f594a0
DestinyJoyner <destinyjoyner@pursuit.org>
2023-07-16 00:54:28 -0400

    recruiter profile job cards updated

--- src/Components/Recruiter/RecruiterJob.js
+++ src/Components/Recruiter/RecruiterJob.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { convertCities } from "../Job/Functions/JobFunctions";
 
-import "./RecruiterJob2.css";
+import "./RecruiterJob.css";
 
 export default function RecruiterJob({ jobObject }) {
   const { title, company, city, users, id } = jobObject;
@@ -11,18 +11,18 @@ export default function RecruiterJob({ jobObject }) {
         <Link to={`/jobs/${id}`} className="recruiter-job-title">
           {title}
         </Link>
-        <span className="recruiter-job-company">{company}</span>
+      </div>
+      <span className="recruiter-job-company">{company}</span>
         <span className="recruiter-job-city">{convertCities(city)}</span>
         <Link
-          className="recruiter-job-applicants grid-center"
+          className="recruiter-job-applicants"
           to={`/jobs/${id}/applicants`}
         >
-          <span>Applicants</span>
-          <span className="applicant-count">
+          <span>Applicants ({users ? users.length : "0"})</span>
+          {/* <span className="applicant-count">
             ({users ? users.length : "0"})
-          </span>
+          </span> */}
         </Link>
-      </div>
     </div>
   );
 }
