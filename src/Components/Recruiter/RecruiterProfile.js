ba8d5734cdb327c5c5834a1ff0f14a55bad0bbea
DestinyJoyner <destinyjoyner@pursuit.org>
2023-07-17 02:55:27 -0400

    recruiterProfile scss format, header restyling

--- src/Components/Recruiter/RecruiterProfile.js
+++ src/Components/Recruiter/RecruiterProfile.js
@@ -8,7 +8,8 @@ import RecruiterIcon from "../../Assets/RECRUITER.png";
 import PlusBlue from "../../Assets/plus-blue.png";
 import PlusGold from "../../Assets/plus-gold.png";
 import { jobCompany } from "../Job/Data/Icons";
-import "./RecruiterProfile.css";
+import convertCompanyForLogo from "../Job/Data/CompanyLogos";
+import "./RecruiterProfile.scss";
 
 export default function RecruiterProfile() {
   const navigate = useNavigate();
@@ -25,44 +26,50 @@ export default function RecruiterProfile() {
           .catch((error) => console.log(error))
       : null;
   }, [recruiterID]);
-  // if (!isRecruiterAcc) {
+
+  if (!isRecruiterAcc) {
     return (
      <NoAccess />
     );
-  // }
-  // return (
-  //   recruiterDetails.id && (
-  //     <div className="recruiter-profile grid-center center">
-  //       <div className="recruiter-profile-top">
-  //         <div>
-  //           <p>
-  //             {recruiterDetails.first_name + " " + recruiterDetails.last_name}
-  //           </p>
-  //           <p>
-  //             {jobCompany}
-  //             <span>{recruiterDetails.organization}</span>
-  //           </p>
-  //         </div>
-  //         <img src={RecruiterIcon} alt="recruiter icon" />
-  //       </div>
-  //       <div className="jobs-posted-header">
-  //         <h2>Jobs Posted ({recruiterDetails.jobs_posted.length})</h2>
-  //         <img
-  //           src={theme === "light" ? PlusBlue : PlusGold}
-  //           alt="plus icon"
-  //           onClick={() => navigate("/jobs/new")}
-  //         />
-  //       </div>
-  //       <div className="recruiter-jobs">
-  //         {recruiterDetails.jobs_posted.length > 0 &&
-  //           recruiterDetails.jobs_posted.map((e) => (
-  //             <RecruiterJob jobObject={e} key={uuidv4()} />
-  //           ))}
-  //       </div>
-  //       <Link className="recruiter-profile-new-job" to="/jobs/new">
-  //         POST A JOB
-  //       </Link>
-  //     </div>
-  //   )
-  // );
+  }
+  return (
+    recruiterDetails.id && (
+      <div className="recruiterProfile grid-center center">
+        <div className="recruiterProfile_header">
+          <span className="recruiterProfile_header_name">{`${recruiterDetails.first_name}${recruiterDetails.last_name}`}
+          </span>
+          <span className="recruiterProfile_header_company">
+            <img 
+            className="recruiterProfile_header_company_logo"
+            src={convertCompanyForLogo(recruiterDetails.organization.toLowerCase())} 
+            alt = "recruiter-company"/>
+            <span className="recruiterProfile_header_company_name">
+              {recruiterDetails.organization}
+            </span>
+            </span>
+          
+          <img src={RecruiterIcon} alt="recruiter icon" />
+        </div>
+
+        
+        <div className="jobs-posted-header">
+          <h2>Jobs Posted ({recruiterDetails.jobs_posted.length})</h2>
+          <img
+            src={theme === "light" ? PlusBlue : PlusGold}
+            alt="plus icon"
+            onClick={() => navigate("/jobs/new")}
+          />
+        </div>
+        <div className="recruiter-jobs">
+          {recruiterDetails.jobs_posted.length > 0 &&
+            recruiterDetails.jobs_posted.map((e) => (
+              <RecruiterJob jobObject={e} key={uuidv4()} />
+            ))}
+        </div>
+        <Link className="recruiter-profile-new-job" to="/jobs/new">
+          POST A JOB
+        </Link>
+      </div>
+    )
+  );
 }
