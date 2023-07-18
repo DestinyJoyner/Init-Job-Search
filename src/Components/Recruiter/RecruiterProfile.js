1f64c4a165e89a678c449b50f9bd6c07f2e9c359
DestinyJoyner <destinyjoyner@pursuit.org>
2023-07-18 00:25:56 -0400

    recruiter profile job list border updated

--- src/Components/Recruiter/RecruiterProfile.js
+++ src/Components/Recruiter/RecruiterProfile.js
@@ -1,19 +1,15 @@
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useRecruiterProvider } from "../../Providers/RecruiterProvider";
 import RecruiterJob from "./RecruiterJob.js";
 import NoAccess from "../App/NoAccess";
-
-import PlusBlue from "../../Assets/plus-blue.png";
-import PlusGold from "../../Assets/plus-gold.png";
 import { recruiter, addJob } from "../Job/Data/Icons";
 import convertCompanyForLogo from "../Job/Data/CompanyLogos";
 import "./RecruiterProfile.scss";
 
 export default function RecruiterProfile() {
-  const navigate = useNavigate();
-  const { recruiterID, axios, API, theme, isRecruiterAcc, setAppHeader } =
+  const { recruiterID, axios, API, isRecruiterAcc, setAppHeader } =
     useRecruiterProvider();
   const [recruiterDetails, setRecruiterDetails] = useState({});
 
@@ -32,7 +28,8 @@ export default function RecruiterProfile() {
   }
   return (
     recruiterDetails.id && (
-      <div className="recruiterProfile grid-center center">
+      <div 
+      className="recruiterProfile grid-center center">
         <div className="recruiterProfile_header">
           <span className="recruiterProfile_header_name">
             {`${recruiterDetails.first_name} ${recruiterDetails.last_name}`}
@@ -47,26 +44,31 @@ export default function RecruiterProfile() {
             )}
             alt="recruiter-company"
           />
-
           <div className="recruiterProfile_header_icon">
             {recruiter}
             <span>Recruiter</span>
           </div>
         </div>
-
+        <hr />
         <div className="recruiterProfile_jobsPosted_header">
-          <h2>Jobs Posted ({recruiterDetails.jobs_posted.length})</h2>
+          <h2>
+            Jobs Posted ({recruiterDetails.jobs_posted.length})
+          </h2>
           <Link to="/jobs/new">{addJob}</Link>
         </div>
+  
         <div className="recruiterProfile_jobsList">
-          {recruiterDetails.jobs_posted.length > 0 ?
-            recruiterDetails.jobs_posted.map((e) => (
-              <RecruiterJob jobObject={e} key={uuidv4()} />
-            )) :
+          {
+          recruiterDetails.jobs_posted.length > 0 ?
+            recruiterDetails.jobs_posted.map(e => 
+              <RecruiterJob jobObject={e} key={uuidv4()} />) :
             <p>No Current Job Postings</p>
           }
         </div>
-        <Link className="recruiterProfile_new-job-link" to="/jobs/new">
+       
+        <Link 
+        className="recruiterProfile_new-job-link" 
+        to="/jobs/new">
           POST A JOB
         </Link>
       </div>
