103c98b5d9a80c12aa9347c78b3270b51004cff6
DestinyJoyner <destinyjoyner@pursuit.org>
2023-07-31 17:53:38 -0400

    slider button functionality implemented using buttons

--- src/Components/App/RadioSliderButton/RadioSliderButtons.js
+++ src/Components/App/RadioSliderButton/RadioSliderButtons.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./RadioSliderButtons.scss";
 
 function RadioSliderButtons({button1, button2, setFunction}) {
@@ -8,39 +9,60 @@ function RadioSliderButtons({button1, button2, setFunction}) {
       setFunction(e.target.value)
     }
 
+    const [slider, setSlider] = useState(false)
+
+
   return (
-    <div className="radioSlider_container flex-center">
-      <div className="radioSlider_buttons"
-      >
-        <input 
-        type="radio" 
-        id="radio-1" 
-        value = {button1}
-        name="radioButtonSlider" 
-        onChange={(event) => handleRadioButton(event)}
-        checked />
+    // <div className="radioSlider_container flex-center">
+    //   <div className="radioSlider_buttons"
+    //   >
+    //     <input 
+    //     type="radio" 
+    //     id="radio-1" 
+    //     value = {button1}
+    //     name="radioButtonSlider" 
+    //     onChange={(event) => handleRadioButton(event)}
+    //     checked />
 
-        <label 
-        className="radioSlider_buttons_label flex-center" 
-        htmlFor="radio-1">
-        {button1}
-        </label>
+    //     <label 
+    //     className="radioSlider_buttons_label flex-center" 
+    //     htmlFor="radio-1">
+    //     {button1}
+    //     </label>
 
-        <input 
-        type="radio" 
-        id="radio-2" 
-        value={button2}
-        name="radioButtonSlider"
-        onChange={(event) => handleRadioButton(event)}
-         />
+    //     <input 
+    //     type="radio" 
+    //     id="radio-2" 
+    //     value={button2}
+    //     name="radioButtonSlider"
+    //     onChange={(event) => handleRadioButton(event)}
+    //      />
         
-        <label className="radioSlider_buttons_label flex-center" 
-        htmlFor="radio-2">
-          {button2}
-        </label>
+    //     <label className="radioSlider_buttons_label flex-center" 
+    //     htmlFor="radio-2">
+    //       {button2}
+    //     </label>
 
-        <span className="radioSlider_slider"></span>
-      </div>
+    //     <span className="radioSlider_slider"></span>
+    //   </div>
+    // </div>
+
+    // buttons test
+
+    <div className="sliderButton_container">
+        <span className={ slider ?"sliderButton_slider slideLeft": "sliderButton_slider slideRight"}></span>
+        <div className="sliderButton_buttons">
+            <button 
+            onClick={()=> setSlider(true)}
+            className={slider ?"sliderButton_buttons_button buttonSelected" : "sliderButton_buttons_button"}>
+                {button1}
+            </button>
+            <button 
+            onClick={() => setFunction(setSlider(false))}
+            className={slider ? "sliderButton_buttons_button" : "sliderButton_buttons_button buttonSelected"}>
+                {button2}
+            </button>
+        </div>
     </div>
   );
 }
