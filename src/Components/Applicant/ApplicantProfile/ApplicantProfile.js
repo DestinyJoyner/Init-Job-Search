b0bddf4e4659874fc8e8791fc58e910b7440a4c6
DestinyJoyner <destinyjoyner@pursuit.org>
2023-08-15 22:48:00 -0400

    created applicant header component

--- src/Components/Applicant/ApplicantProfile/ApplicantProfile.js
+++ src/Components/Applicant/ApplicantProfile/ApplicantProfile.js
@@ -3,7 +3,7 @@ import { useUserProvider } from "../../../Providers/UserProvider";
 import { useContextProvider } from "../../../Providers/Provider";
 import { useNavProvider } from "../../../Providers/NavProvider";
 import NoAccess from "../../App/NoAccess.js";
-import SkillsComponent from "../../Job/SkillsComponent";
+import ApplicantProfileHeader from "../ApplicantProfilePage/ApplicantProfileHeader";
 
 import "./ApplicantProfile.scss";
 
@@ -12,10 +12,9 @@ function ApplicantProfile(props) {
     useUserProvider();
   const { userID, isRecruiterAcc, loading, setLoading } = useContextProvider();
   const { setAppHeader } = useNavProvider();
-console.log(applicantDetails)
   // variables
-  const firstInitial = applicantDetails.id ? applicantDetails["first_name"].charAt(0): "";
-  const lastInitial = applicantJobs.id ?applicantDetails["last_name"].charAt(0): "";
+  const {id, education, project_one, project_two} = applicantDetails
+
 
   useEffect(() => {
     if (isRecruiterAcc && !userID) {
@@ -24,9 +23,9 @@ console.log(applicantDetails)
       setAppHeader("Profile");
     }
   }, []);
-  
+
   useEffect(() => {
-    if(applicantDetails.id){
+    if(id){
         setLoading(false)
     }
   },[applicantDetails])
@@ -38,22 +37,10 @@ console.log(applicantDetails)
   return (
     !loading && (
       <div className="applicantProfile grid-center">
-        <section className="applicantProfile_header">
-          <div className="applicantProfile_header_icon">
-            {firstInitial}
-            {lastInitial}
-          </div>
-          <div className="applicantProfile_header_details">
-            <span className="applicantProfile_header_details_name">{applicantDetails.first_name} {applicantDetails.last_name}</span>
-            <span className="applicantProfile_header_details_role">ROLE</span>
-            <SkillsComponent justList={true}
-            skillsArr={applicantSkillIds}/>
-          </div>
-          <div className="applicantProfile_header_about">
-            <span className="applicantProfile_header_about_header">About Me:</span>
-            <p className="applicantProfile_header_about_text">{applicantDetails.bio} </p>
-          </div>
-        </section>
+        <ApplicantProfileHeader 
+        applicantDetails={applicantDetails}
+        applicantSkills={applicantSkillIds}/>
+       
       </div>
     )
   );
