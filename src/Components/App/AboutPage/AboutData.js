d017aef4771bd5bbde2221ecf653f5b81224dc3c
DestinyJoyner <destinyjoyner@pursuit.org>
2023-08-21 20:40:30 -0400

    about me updated with header and sliderbuttons

--- src/Components/App/AboutPage/AboutData.js
+++ src/Components/App/AboutPage/AboutData.js
@@ -7,7 +7,7 @@ export const team = {
     name: "Destiny Joyner",
     pronoun: "She/Her",
     img: DJphoto,
-    role: "Front/Back-End Developer",
+    role: "Full Stack Web Developer",
     bio:
     <>Born and raised in New York City, Destiny always had a flare for solving math and logic puzzles.<br/> She is intrigued by the vast opportunities that virtual augmentation enables users to experience. <br/> By continuously expanding her programming knowledge, Destiny aims to build immersive worlds inside of the virtual universe.
     </>,
@@ -20,7 +20,7 @@ export const team = {
  
   2: {
     id: 2,
-    name: "Click on an icon to learn more!",
+    name: "About the Application",
     img: group,
     bio:<>
     <span className="about-init">inIT</span> is a mobile-first web application designed for recent college and boot camp graduates looking for their first role in tech. <br/><br/> <span className="about-init">inIT's</span> frontend was developed using <span className="about-react">React</span> and <span className="about-css">CSS3</span>, with no other frameworks or libraries. <br/><br/> <span className="about-init">inIT</span> utilizes a custom API built in an <span className="about-express">Express/Node JS</span> environment, also using <span className="about-postgres">PostgreSQL</span> for the database structure, and requiring <span className="about-bcrypt">BCrypt</span> and <span className="about-jwt">JWT</span> packages for it's authentication processes.</>,
