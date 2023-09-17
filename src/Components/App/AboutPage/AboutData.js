7d3fe2fbc3d5fbcfdc58b88a012eea76e3ba1f1d
DestinyJoyner <destinyjoyner@pursuit.org>
2023-09-17 14:41:26 -0400

    desktop footer done, styled, links

--- src/Components/App/AboutPage/AboutData.js
+++ src/Components/App/AboutPage/AboutData.js
@@ -11,8 +11,7 @@ export const team = {
     bio:
     <>Born and raised in New York City, Destiny always had a flare for solving math and logic puzzles.<br/> She is intrigued by the vast opportunities that virtual augmentation enables users to experience. <br/> By continuously expanding her programming knowledge, Destiny aims to build immersive worlds inside of the virtual universe.
     </>,
-    bio2: <>As a full stack web developer from Harlem New York, my journey into coding began with customizing my Myspace page back in high school. This led me to attend the Rose Hulman Institute of Technology where I studied computer science. <br/>
-    In June 2023, I completed Pursuit, an intensive Google funded, 1 year software engineering program where I honed my web development skills using JavaScript, NodeJs, and React. 
+    bio2: <>My name is Destiny Joyner, a software engineer specializing in full stack web development. Coming from a non-traditional training background, it was a hassle finding entry level tech opportunities.  <br/>I built inIT to expedite the job search process by directly connecting recruiters looking to fill positions with entry level software developers looking to land their first role in the tech industry.
     </> ,
     links: [
       "https://github.com/DestinyJoyner",
@@ -27,6 +26,12 @@ export const team = {
     img: group,
     bio:<>
     <span className="about-init">inIT</span> is a mobile-first web application designed for recent college and boot camp graduates looking for their first role in tech. <br/><br/> <span className="about-init">inIT's</span> frontend was developed using <span className="about-react">React</span> and <span className="about-css">CSS3</span>, with no other frameworks or libraries. <br/><br/> <span className="about-init">inIT</span> utilizes a custom API built in an <span className="about-express">Express/Node JS</span> environment, also using <span className="about-postgres">PostgreSQL</span> for the database structure, and requiring <span className="about-bcrypt">BCrypt</span> and <span className="about-jwt">JWT</span> packages for it's authentication processes.</>,
+    bio2: <span> inIt is a job search web engine  designed for recent college and boot camp graduates looking for their first role in tech. Some of inIT's features include a fully responsive web design, extensive search filtering options, UX/UI that supports both recruiter and applicant roles with full CRUD functionality on both profile and job postings.</span>,
+    build: <span>inIT is a PERN stack application styled using SCSS/CSS3. inIt further utilizes BCrypt and JWT packages for it's authentication processes.</span>,
     links: [1,2,3,90,91,0],
   },
 };
+
+/* 
+In June 2023, I completed <a href="https://www.pursuit.org/mission-vision" target="_blank">Pursuit's</a>, full stack software engineering program where I honed my web development skills using JavaScript, NodeJs, and React. 
+*/
--- src/Components/App/Footer/DesktopFooter/DesktopFooter.js
+++ src/Components/App/Footer/DesktopFooter/DesktopFooter.js
@@ -1,30 +1,60 @@
+import SkillsComponent from "../../../Job/SkillsComponent";
 import { team } from "../../AboutPage/AboutData";
-import developerImage from "../../../../Assets/DJphoto.png"
-import "./DesktopFooter.scss"
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+import developerImage from "../../../../Assets/DJphoto.png";
+import pursuit from "../../../../Assets/pursuit-blue-logo.jpg"
+import "./DesktopFooter.scss";
 
 function DesktopFooter() {
-    const developerBio = team["1"].bio2
-    return (
-        <div className="desktopFooter">
-            <section className="desktopFooter_about">
-            <h2> About inIT</h2>
-            </section>
-            
+  const developerBio = team["1"].bio2;
+  const initBio = team["2"].bio2;
+  const initTech = team["2"].build;
+  const initSkills = team["2"].links;
 
-            <section className="desktopFooter_developer">
-            <h2>The Developer</h2>
-            <img src={developerImage} alt ="developer-image" />
-            <span className="desktopFooter_developer_bio">
-                {developerBio}
-            </span>
+  return (
+    <div className="desktopFooter">
 
-            <div className="desktopFooter_developer_links">links</div>
+      <section className="desktopFooter_about">
+        <h2>About inIT</h2>
+        <span className="desktopFooter_about_bio">{initBio}</span>
+        <h2>Technologies Used</h2>
+        <span className="desktopFooter_about_tech">
+          {initTech}
+          <SkillsComponent justList={true} skillsArr={initSkills} />
+        </span>
+      </section>
 
-            <span className="desktopFooter_developer_name">Destiny Joyner</span>
-            </section>
-            
-        </div>
-    );
+      <section className="desktopFooter_developer">
+        <h2>About The Developer</h2>
+        <span className="desktopFooter_developer_bio">
+          {developerBio}
+          <img src={developerImage} alt="developer-image" />
+        </span>
+        <h2 className="desktopFooter_developer_contact">Contact Me</h2>
+
+        <section className="desktopFooter_developer_links">
+          <a href="https://github.com/DestinyJoyner" target="blank">
+            <BsGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/destiny-joyner-934846243/"
+            target="blank"
+          >
+            <BsLinkedin />
+          </a>
+          <a href="mailto:destinyjoyner@pursuit.org" target="_blank">
+            <SiGmail />
+          </a>
+          <a href="https://www.pursuit.org/mission-vision" target="_blank">
+          <img className="desktopFooter_developer_links_pursuit" src={pursuit} alt="pursuit-logo" />
+        </a>
+        </section>
+
+      </section>
+
+    </div>
+  );
 }
 
 export default DesktopFooter;
