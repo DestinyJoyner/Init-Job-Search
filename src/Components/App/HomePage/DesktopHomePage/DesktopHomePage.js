9091f188d08591663a4e77b7a1f337473c73a2ee
DestinyJoyner <destinyjoyner@pursuit.org>
2023-09-16 22:25:39 -0400

    about layout fixed, footer about tbd

--- src/Components/App/HomePage/DesktopHomePage/DesktopHomePage.js
+++ src/Components/App/HomePage/DesktopHomePage/DesktopHomePage.js
@@ -1,4 +1,5 @@
 import logo from "../../../../Assets/LOGO.png";
+import stockImage2 from "../Images/init-stock(3).jpg";
 import "./DesktopHomePage.scss";
 
 function DesktopHomePage(props) {
@@ -21,29 +22,60 @@ function DesktopHomePage(props) {
       </section>
 
       <section className="desktopHome_content_init">
+        {/* <img 
+        className="desktopHome_content_init_stock2"
+        src={stockImage2} alt ="stock-image" /> */}
 
         <section className="desktopHome_content_init_one">
-        <span className="desktopHome_content_init_question">
-            Why Choose inIT?
-        </span>
-        <ul className="desktopHome_content_init_answer">
-           <li>40% of software developers struggle to find work within a year of boot camp or college completion</li> 
-           <li>Identifying jobs for entry-level developers with little to no experience can be difficult</li>
-           <li>Other job search engines, harbor a "social media" environment that can prove tedious and overwhelming </li>
-        </ul>
+          <div className="desktopHome_content_init_one_text">
+            <span className="desktopHome_content_init_question">
+              Why Choose inIT?
+            </span>
+            <ul className="desktopHome_content_init_answer">
+              <li>
+                40% of software developers struggle to find work within a year
+                of boot camp or college completion
+              </li>
+              <li>
+                Identifying jobs for entry-level developers with little to no
+                experience can be difficult
+              </li>
+              <li>
+                Other job search engines, harbor a "social media" environment
+                that can prove tedious and overwhelming{" "}
+              </li>
+            </ul>
+          </div>
+
+          <img
+            className="desktopHome_content_init_stock2"
+            src={stockImage2}
+            alt="stock-image"
+          />
         </section>
 
         <section className="desktopHome_content_init_two">
-        <span className="desktopHome_content_init_question">
-            Our Mission?
-        </span>
-        <ul className="desktopHome_content_init_answer">
-           <li>40% of software developers struggle to find work within a year of boot camp or college completion</li> 
-           <li>Identifying jobs for entry-level developers with little to no experience can be difficult</li>
-           <li>Other job search engines, harbor a "social media" environment that can prove tedious and overwhelming </li>
-        </ul>
+          <img
+            className="desktopHome_content_init_stock2"
+            src={stockImage2}
+            alt="stock-image"
+          />
+          <div className="desktopHome_content_init_one_text">
+            <span className="desktopHome_content_init_question">
+              Our Mission?
+            </span>
+            <ul className="desktopHome_content_init_answer">
+              <li>
+                40% of software developers struggle to find work within a year
+                of boot camp or college completion
+              </li>
+              <li>
+                Identifying jobs for entry-level developers with little to no
+                experience can be difficult
+              </li>
+            </ul>
+          </div>
         </section>
-       
       </section>
 
       <section className="desktopHome_content">About</section>
