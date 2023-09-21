f29bce8515b72365fa52ac05754c048f72a88e02
DestinyJoyner <destinyjoyner@pursuit.org>
2023-09-21 00:01:10 -0400

    desktop login page set up

---
+++ src/Components/Login/DesktopLoginPage/DesktopLoginPage.js
@@ -0,0 +1,15 @@
+import LoginPage from "../LoginPage/LoginPage.js"
+import "./DesktopLoginPage.scss"
+
+function DesktopLoginPage(props) {
+    return (
+        <div className="desktopLoginPage">
+            <section className="desktopLoginPage_image">
+                
+            </section>
+            <LoginPage />
+        </div>
+    );
+}
+
+export default DesktopLoginPage;
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,19 @@
+import { useWindowSizeProvider } from "../Providers/WindowSizeProvider.js";
+import DesktopLoginPage from "../Components/Login/DesktopLoginPage/DesktopLoginPage.js";
 import LoginPage from "../Components/Login/LoginPage/LoginPage.js";
 import RecruiterProvider from "../Providers/RecruiterProvider.js";
 
 export default function Login() {
+  const { isDesktopView } = useWindowSizeProvider()
+
   return (
     <RecruiterProvider>
-      <LoginPage/>
+      {
+        isDesktopView ?
+        <DesktopLoginPage /> :
+        <LoginPage/>
+      }
+      
     </RecruiterProvider>
   );
 }
