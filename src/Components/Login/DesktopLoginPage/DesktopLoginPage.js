a98cb7ebcad2d7fdfa8e1ba39a3cfa307cddc3e2
DestinyJoyner <destinyjoyner@pursuit.org>
2023-09-27 21:58:43 -0400

    temporary login page desktop style tb changed

--- src/Components/Login/DesktopLoginPage/DesktopLoginPage.js
+++ src/Components/Login/DesktopLoginPage/DesktopLoginPage.js
@@ -1,13 +1,25 @@
 import LoginPage from "../LoginPage/LoginPage.js"
+import goldFace from "../../../Assets/gold-face.png"
+
 import "./DesktopLoginPage.scss"
 
 function DesktopLoginPage(props) {
     return (
-        <div className="desktopLoginPage">
-            <section className="desktopLoginPage_image">
+        <div className="desktopLoginPage">  
+            <section className="desktopLoginPage_content">
+                <div className="desktopLoginPage_content_left">
+                    <div className="desktopLoginPage_content_left_transparent grid-center">
+                    <span>Welcome Back<br/> To<br/> inIT!</span>
+                    </div>
+                    {/* <img src={goldFace} alt="face-circuit" className="desktopLoginPage_content_left_image" /> */}
+                    
+            
+                   
                 
+                </div>
+                <LoginPage />
             </section>
-            <LoginPage />
+            
         </div>
     );
 }
