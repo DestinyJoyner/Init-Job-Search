a667d9174d93864944730b003c7362251aba273b
DestinyJoyner <destinyjoyner@pursuit.org>
2023-09-28 00:04:20 -0400

    login page style updated

--- src/Components/Login/DesktopLoginPage/DesktopLoginPage.js
+++ src/Components/Login/DesktopLoginPage/DesktopLoginPage.js
@@ -1,27 +1,45 @@
-import LoginPage from "../LoginPage/LoginPage.js"
-import goldFace from "../../../Assets/gold-face.png"
-
-import "./DesktopLoginPage.scss"
+import LoginPage from "../LoginPage/LoginPage.js";
+import "./DesktopLoginPage.scss";
 
 function DesktopLoginPage(props) {
-    return (
-        <div className="desktopLoginPage">  
-            <section className="desktopLoginPage_content">
-                <div className="desktopLoginPage_content_left">
-                    <div className="desktopLoginPage_content_left_transparent grid-center">
-                    <span>Welcome Back<br/> To<br/> inIT!</span>
-                    </div>
-                    {/* <img src={goldFace} alt="face-circuit" className="desktopLoginPage_content_left_image" /> */}
-                    
-            
-                   
-                
-                </div>
-                <LoginPage />
-            </section>
-            
-        </div>
-    );
+  return (
+    <div className="desktopLoginPage">
+      <section className="desktopLoginPage_content">
+        <section className="desktopLoginPage_content_left grid-center">
+
+            <div className="desktopLoginPage_content_left_header">
+              <span className="desktopLoginPage_content_left_header_top">Welcome Back To inIT</span>
+              <span className="desktopLoginPage_content_left_header_bottom">Streamline Your Path into Tech
+                </span>
+            </div>
+
+            <div className="desktopLoginPage_content_left_subHeader">
+                <span className="desktopLoginPage_content_left_subHeader_top">
+                Your Entry to Entry-Level Jobs
+                </span>
+                <span className="desktopLoginPage_content_left_subHeader_bottom">
+                inIT aims to connect employers looking to fill entry level positions, with job seekers on the hunt for their first role in the tech industry  
+                </span>
+            </div>
+
+            <span className="desktopLoginPage_content_left_featureHeader">
+                Features:
+            </span>
+
+            <ul className="desktopLoginPage_content_left_featureList">
+                <li>No prior work experience</li>
+                <li>Only Entry-Level positions</li>
+                <li>Showcase Your Personal Projects</li>
+                <li>Track Your Applications</li>
+                <li>Search Jobs by skill, location, and remote options</li>
+                <li>Manageable, concise profiles</li>
+
+            </ul>
+        </section>
+        <LoginPage />
+      </section>
+    </div>
+  );
 }
 
 export default DesktopLoginPage;
