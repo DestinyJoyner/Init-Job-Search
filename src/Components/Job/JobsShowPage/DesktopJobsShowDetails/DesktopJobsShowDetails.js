6251cc0f5dfbee17e5e15d311996246ab67a85b9
DestinyJoyner <destinyjoyner@pursuit.org>
2023-10-17 20:47:26 -0400

    job show page doughnut chart and related jobs features added and styled, functioning

--- src/Components/Job/JobsShowPage/DesktopJobsShowDetails/DesktopJobsShowDetails.js
+++ src/Components/Job/JobsShowPage/DesktopJobsShowDetails/DesktopJobsShowDetails.js
@@ -1,71 +1,110 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import { useContextProvider } from "../../../../Providers/Provider";
 import DesktopSkillsComponent from "../../../DesktopSkillsComponent/DesktopSkillsComponent";
 import JobsShowActionButtons from "../JobsShowActionButtons/JobsShowActionButtons";
 import SkillDoughnutChart from "../../../SkillDoughnutChart/SkillDoughnutChart";
-import "./DesktopJobsShowDetails.scss"
+import { jobLocation } from "../../../App/Data/Icons";
 
-function DesktopJobsShowDetails({jobDetails, applied, setApplied, desktopJobSkills}) {
-    const TASK = process.env.REACT_APP_TASK_BREAK;
-    const { details, tasks, city} = jobDetails
-    
-    return (
-        <div className='desktopJobShowDetails'>
-             <DesktopSkillsComponent desktopJobSkills={desktopJobSkills} />
-            <JobsShowActionButtons applied={applied}
-        setApplied={setApplied} />
+import "./DesktopJobsShowDetails.scss";
 
-        <section className="desktopJobShowDetails_text">
-            < section className="desktopJobShowDetails_text_details">
-            <div className="desktopJobShowDetails_text_details_description">
-                <span className="desktopJobShowDetails_text_header">
-                    Description
-                </span>
-                <span className="desktopJobShowDetails_text_details_description_content">
-                    {details}
-                </span>
-            </div>
+function DesktopJobsShowDetails({
+  jobDetails,
+  applied,
+  setApplied,
+  desktopJobSkills,
+}) {
+  const { API, axios } = useContextProvider();
+  const TASK = process.env.REACT_APP_TASK_BREAK;
+  const { details, tasks, city } = jobDetails;
 
-            <hr className="desktopJobShowDetails_text_details_divider"/>
+  const [relatedJobs, setRelatedJobs] = useState([]);
 
-            <div className="desktopJobShowDetails_text_details_tasks">
+  useEffect(() => {
+    if (city) {
+      const convertCity = city.replaceAll(" ", "").toLowerCase();
+      axios
+        .get(`${API}/jobs?start=0&limit=4&city=${convertCity}`)
+        .then(({ data }) => {
+          data.shift();
+          const filterRelatedJobs = data.filter(
+            ({ id }) => id !== jobDetails.id
+          );
+          setRelatedJobs(filterRelatedJobs);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [jobDetails]);
+  
+  return (
+    <div className="desktopJobShowDetails">
+      <DesktopSkillsComponent desktopJobSkills={desktopJobSkills} />
+      <JobsShowActionButtons applied={applied} setApplied={setApplied} />
+
+      <section className="desktopJobShowDetails_text">
+        <section className="desktopJobShowDetails_text_details">
+          <div className="desktopJobShowDetails_text_details_description">
             <span className="desktopJobShowDetails_text_header">
-                Tasks
+              Description
             </span>
-            <ul className="desktopJobShowDetails_text_details_tasks_content">
-            {
-            jobDetails.tasks &&
-            jobDetails.tasks.split(`${TASK}`).map((el) => {
-              if (el) {
-                return (
-                  <li key={uuidv4()}>
-                    <span>{el}</span>
-                  </li>)}
-           })
-        }
-            </ul>
-            </div>
-            </section>
+            <span className="desktopJobShowDetails_text_details_description_content">
+              {details}
+            </span>
+          </div>
 
-            <aside className="desktopJobShowDetails_text_aside">
-                <SkillDoughnutChart />
-                <div className="desktopJobShowDetails_text_aside_relatedJobs">
-                    <span className="desktopJobShowDetails_text_aside_relatedJobs_header">
-                        Jobs Near {city}
-                    </span>
-                </div>
-            
-            </aside>
-            <Link
-        className="desktopJobShowDetails_backButton" 
-        to="/jobs">{"<"} 
-        <span>Back to Jobs</span></Link>
-           
+          <hr className="desktopJobShowDetails_text_details_divider" />
+
+          <div className="desktopJobShowDetails_text_details_tasks">
+            <span className="desktopJobShowDetails_text_header">Tasks</span>
+            <ul className="desktopJobShowDetails_text_details_tasks_content">
+              {jobDetails.tasks &&
+                jobDetails.tasks.split(`${TASK}`).map((el) => {
+                  if (el) {
+                    return (
+                      <li key={uuidv4()}>
+                        <span>{el}</span>
+                      </li>
+                    );
+                  }
+                })}
+            </ul>
+          </div>
         </section>
-       
-          
-        </div>
-    );
+
+        <aside className="desktopJobShowDetails_text_aside">
+          <SkillDoughnutChart />
+
+          <div className="desktopJobShowDetails_text_aside_relatedJobs">
+            <span className="desktopJobShowDetails_text_aside_relatedJobs_header">
+              {jobLocation} Jobs Near {city}
+            </span>
+            <ul className="desktopJobShowDetails_text_aside_relatedJobs_list">
+              {relatedJobs.length > 0 ? (
+                relatedJobs.map(({ title, company, id }) => (
+                  <li key={uuidv4()}>
+                    <Link to={`/jobs/${id}`}>
+                      {title}, <br />
+                      {company}
+                    </Link>
+                  </li>
+                ))
+              ) : (
+                <span className="desktopJobShowDetails_text_aside_relatedJobs_list_none">
+                  No Jobs to Display
+                </span>
+              )}
+            </ul>
+          </div>
+
+          <Link className="desktopJobShowDetails_backButton" to="/jobs">
+            {"<"}
+            <span>Back to Jobs</span>
+          </Link>
+        </aside>
+      </section>
+    </div>
+  );
 }
 
 export default DesktopJobsShowDetails;
--- src/Components/SkillDoughnutChart/SkillDoughnutChart.js
+++ src/Components/SkillDoughnutChart/SkillDoughnutChart.js
@@ -1,51 +1,58 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend} from "chart.js"
-import {Doughnut} from "react-chartjs-2"
-import { useUserProvider } from "../../Providers/UserProvider"
-import { useJobProvider } from "../../Providers/JobProvider"
-import "./SkillDoughnutChart.scss"
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import { useUserProvider } from "../../Providers/UserProvider";
+import { useJobProvider } from "../../Providers/JobProvider";
+import SkillsComponent from "../Job/SkillsComponent";
+import "./SkillDoughnutChart.scss";
 
-ChartJS.register(ArcElement, Tooltip, Legend)
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 function SkillDoughnutChart() {
-    const { applicantSkillIds } = useUserProvider()
-    const { jobSkills } = useJobProvider()
-    const skillMatches = applicantSkillIds.filter(el => jobSkills.includes(el))
-    const skillMatchPercent = Math.floor((skillMatches.length / jobSkills.length) * 100)
- 
-    const data ={
-        labels:["Skill Compatibilty"],
-        datasets:[{
-            label:'Skill Match',
-            data:[skillMatches.length,jobSkills.length-skillMatches.length],
-            backgroundColor:["#ffde59","#41cdbc"],
-            borderColor:[ "#ffde59", "#41cdbc"]
-        }]
-    }
-    const options = {
-        plugins: {
-            tooltip: {
-              enabled: false
-            },
-            // maintainAspectRatio: false
-            // legend: {
-            //     display: false
-            // }
-        }
-        
-    }
-    return (
-        <div className="skillDoughnutChart">
-            <Doughnut
-            data={data}
-            options={options}>
+  const { applicantSkillIds } = useUserProvider();
+  const { jobSkills } = useJobProvider();
+  const skillMatches = applicantSkillIds.filter((el) => jobSkills.includes(el));
+  const skillMatchPercent = Math.floor(
+    (skillMatches.length / jobSkills.length) * 100
+  );
 
-            </Doughnut>
-            <span className="skillDoughnutChart_percentage">
-            {skillMatchPercent}%
-            </span>
-            
-        </div>
-    );
+  const data = {
+    labels: ["Skill Compatibilty"],
+    datasets: [
+      {
+        label: "Skill Match",
+        data: [skillMatches.length, jobSkills.length - skillMatches.length],
+        backgroundColor: ["#ffde59", "#41cdbc"],
+        borderColor: ["#ffde59", "#41cdbc"],
+      },
+    ],
+  };
+  const options = {
+    responsive: false,
+    maintainAspectRatio: false,
+    plugins: {
+      tooltip: {
+        enabled: false,
+      },
+      // maintainAspectRatio: false,
+      legend: {
+        display: false,
+      },
+    },
+  };
+  return (
+    <div className="skillDoughnutChart">
+      <h3>You have {skillMatchPercent}% Skill Compability with this Job </h3>
+      <>
+        <Doughnut data={data} options={options}></Doughnut>
+      </>
+
+      <span className="skillDoughnutChart_percentage">
+        {skillMatchPercent}%
+      </span>
+
+      <SkillsComponent justList={true} skillsArr={skillMatches} />
+    </div>
+  );
 }
 
 export default SkillDoughnutChart;
