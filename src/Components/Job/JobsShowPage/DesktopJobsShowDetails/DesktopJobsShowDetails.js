ca02796f80f6ea1ddde151059f79cf326368a42c
DestinyJoyner <destinyjoyner@pursuit.org>
2024-02-05 22:17:38 -0500

    job show header background image style fix

--- src/Components/Job/JobsShowPage/DesktopJobsShowDetails/DesktopJobsShowDetails.js
+++ src/Components/Job/JobsShowPage/DesktopJobsShowDetails/DesktopJobsShowDetails.js
@@ -1,13 +1,9 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
-
 import DesktopSkillsComponent from "../../../DesktopSkillsComponent/DesktopSkillsComponent";
 import JobsShowActionButtons from "../JobsShowActionButtons/JobsShowActionButtons";
 import DesktopJobShowDetailsAside from "./DesktopJobShowDetailsAside/DesktopJobShowDetailsAside";
 import DesktopJobShowCompanyDetails from "./DesktopJobShowCompanyDetails/DesktopJobShowCompanyDetails";
-import { jobLocation } from "../../../App/Data/Icons";
-
 import "./DesktopJobsShowDetails.scss";
 
 function DesktopJobsShowDetails({
@@ -16,10 +12,9 @@ function DesktopJobsShowDetails({
   setApplied,
   desktopJobSkills,
 }) {
-
   const TASK = process.env.REACT_APP_TASK_BREAK;
   const { details, tasks } = jobDetails;
-  
+
   return (
     <div className="desktopJobShowDetails">
       <DesktopSkillsComponent desktopJobSkills={desktopJobSkills} />
@@ -41,8 +36,8 @@ function DesktopJobsShowDetails({
           <div className="desktopJobShowDetails_text_details_tasks">
             <span className="desktopJobShowDetails_text_header">Tasks</span>
             <ul className="desktopJobShowDetails_text_details_tasks_content">
-              {jobDetails.tasks &&
-                jobDetails.tasks.split(`${TASK}`).map((el) => {
+              {tasks &&
+                tasks.split(`${TASK}`).map((el) => {
                   if (el) {
                     return (
                       <li key={uuidv4()}>
@@ -57,23 +52,21 @@ function DesktopJobsShowDetails({
           <hr className="desktopJobShowDetails_text_details_divider" />
 
           <div className="desktopJobShowDetails_text_details_company">
-            <span className="desktopJobShowDetails_text_header">About This Company </span>
+            <span className="desktopJobShowDetails_text_header">
+              About This Company{" "}
+            </span>
 
-          <DesktopJobShowCompanyDetails
-          jobDetails={jobDetails} />
-
+            <DesktopJobShowCompanyDetails jobDetails={jobDetails} />
           </div>
         </section>
 
         <DesktopJobShowDetailsAside jobDetails={jobDetails} />
-
-     
       </section>
 
       <Link className="desktopJobShowDetails_backButton" to="/jobs">
-            {"<"}
-            <span>Back to Jobs</span>
-          </Link>
+        {"<"}
+        <span>Back to Jobs</span>
+      </Link>
     </div>
   );
 }
