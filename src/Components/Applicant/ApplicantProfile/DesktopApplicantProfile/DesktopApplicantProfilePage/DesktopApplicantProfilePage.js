50895d3d5f46308a72afccb24af36b880a4dec94
DestinyJoyner <destinyjoyner@pursuit.org>
2024-03-27 15:00:39 -0400

    logo images added, logo banner component for profile page added

--- src/Components/Applicant/ApplicantProfile/DesktopApplicantProfile/DesktopApplicantProfilePage/DesktopApplicantProfilePage.js
+++ src/Components/Applicant/ApplicantProfile/DesktopApplicantProfile/DesktopApplicantProfilePage/DesktopApplicantProfilePage.js
@@ -4,6 +4,7 @@ import { useUserProvider } from "../../../../../Providers/UserProvider.js";
 import ApplicantProfileHeader from "../../ApplicantProfileHeader/ApplicantProfileHeader.js"
 import ApplicantProfileDetails from "../../ApplicantProfileDetails.js";
 import ApplicantProfileAppliedJobs from "../../ApplicantProfileAppliedJobs.js";
+import LogoBanner from "../../../../App/LogoBanner/LogoBanner.js";
 import "./DesktopApplicantProfilePage.scss"
 
 function DesktopApplicantProfilePage() {
@@ -11,19 +12,27 @@ function DesktopApplicantProfilePage() {
     const {applicantDetails, applicantSkillIds, applicantJobs} = useUserProvider()
 
     useEffect(() => {
+       
         if(applicantDetails.id){
             setLoading(false)
         }
-    },[loading])
+    },[applicantDetails])
     
     return (
         <div className='desktopApplicantProfilePage'>
+            <section className="desktopApplicantProfilePage_banner">
+                <LogoBanner />
+            </section>
+
+            <section className="desktopApplicantProfilePage_content">
             <ApplicantProfileHeader applicantDetails={applicantDetails}
             applicantSkills={applicantSkillIds} />
         
         <ApplicantProfileAppliedJobs applicantJobs={applicantJobs} />
 
         <ApplicantProfileDetails applicantDetails={applicantDetails} />
+            </section>
+            
         </div>
     );
 }
