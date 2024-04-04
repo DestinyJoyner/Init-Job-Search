4817b5c3eb529b432f0e400985e398090544519f
DestinyJoyner <destinyjoyner@pursuit.org>
2024-04-04 18:50:17 -0400

    recruiter top jobs on page

--- src/Components/Recruiter/RecruiterProfile/DesktopRecruiterProfile/DesktopRecruiterProfile.js
+++ src/Components/Recruiter/RecruiterProfile/DesktopRecruiterProfile/DesktopRecruiterProfile.js
@@ -2,54 +2,72 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useRecruiterProvider } from "../../../../Providers/RecruiterProvider";
 import { useContextProvider } from "../../../../Providers/Provider";
-import LogoBanner from "../../../App/LogoBanner/LogoBanner"
+import LogoBanner from "../../../App/LogoBanner/LogoBanner";
 import RecruiterProfileHeader from "../RecruiterProfileHeader";
+import DesktopRecruiterTopJobs from "./DesktopRecruiterTopJobs/DesktopRecruiterTopJobs";
 
-import "./DesktopRecruiterProfile.scss"
+
+import "./DesktopRecruiterProfile.scss";
 
 function DesktopRecruiterProfile(props) {
-    const { isRecruiterAcc, setLoading, loading, recruiterID, axios, API } = useContextProvider();
-    const { recruiterDetails, recruiterJobs, setRecruiterJobs, recruiterJobsWithUsers, setRecruiterJobsWithUsers } = useRecruiterProvider()
+  const { isRecruiterAcc, setLoading, loading, recruiterID, axios, API } =
+    useContextProvider();
+  const {
+    recruiterDetails,
+    recruiterJobs,
+    setRecruiterJobs,
+    recruiterJobsWithUsers,
+    setRecruiterJobsWithUsers,
+  } = useRecruiterProvider();
 
-    const [companyDetails, setCompanyDetails] = useState({})
+  const [companyDetails, setCompanyDetails] = useState({});
 
-    const totalApplicants = recruiterJobsWithUsers.reduce((acc,{users}) =>{
-        return acc+= users.length
-    },0)
-useEffect(() => {
-    if(recruiterDetails.id){
-        const companyName = recruiterDetails.organization.replaceAll(" ","")
-        axios.get(`${API}/company/${companyName.toLowerCase()}`)
-        .then(({data}) => setCompanyDetails(data))
-        .catch(err => console.log(err))
+  const totalApplicants = recruiterJobsWithUsers.reduce((acc, { users }) => {
+    return (acc += users.length);
+  }, 0);
+  useEffect(() => {
+    if (recruiterDetails.id) {
+      const companyName = recruiterDetails.organization.replaceAll(" ", "");
 
-        setLoading(false)
+      axios
+        .get(`${API}/company/${companyName.toLowerCase()}`)
+        .then(({ data }) => setCompanyDetails(data))
+        .catch((err) => console.log(err));
+
+      setLoading(false);
     }
-},[recruiterDetails])
+  }, [recruiterDetails]);
 
-    return (
-        !loading&&
-        <div className="desktopRecruiterProfilePage">
-            <LogoBanner />
+  return (
+    !loading && (
+      <div className="desktopRecruiterProfilePage">
+        <LogoBanner />
 
-            <section className="desktopRecruiterProfilePage_content">
-            <RecruiterProfileHeader recruiterDetails={recruiterDetails} recruiterJobs={recruiterJobs} totalApplicants={totalApplicants} />
+        <section className="desktopRecruiterProfilePage_content">
+          <RecruiterProfileHeader
+            recruiterDetails={recruiterDetails}
+            recruiterJobs={recruiterJobs}
+            totalApplicants={totalApplicants}
+          />
 
-            <section className="desktopRecruiterProfilePage_content_bottomHeader">
-        
-          <div className="desktopRecruiterProfilePage_content_bottomHeader_newJob">
-          <Link to="/jobs/new">Post Opportunity</Link>
-          </div>
+          <section className="desktopRecruiterProfilePage_content_bottomHeader">
+            <div className="desktopRecruiterProfilePage_content_bottomHeader_newJob">
+              <Link to="/jobs/new">Post Opportunity</Link>
+            </div>
 
-          <p className="desktopRecruiterProfilePage_content_bottomHeader_about"> {companyDetails.company_description}
-          </p>
-
+            <p className="desktopRecruiterProfilePage_content_bottomHeader_about">
+              {" "}
+              {companyDetails.company_description}
+            </p>
           </section>
-            </section>
-            
-            
-        </div>
-    );
+
+          <DesktopRecruiterTopJobs recruiterJobs={recruiterJobs}/>
+          
+        </section>
+
+      </div>
+    )
+  );
 }
 
 export default DesktopRecruiterProfile;
---
+++ src/Components/Recruiter/RecruiterProfile/DesktopRecruiterProfile/DesktopRecruiterTopJobs/DesktopRecruiterTopJobs.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { getTwoHighestAppliedToJobs } from "../../../../Functions/RecruiterFunctions/RecruiterProfileFunctions";
+import { v4 as uuidv4 } from "uuid";
+import RecruiterProfileTopJobs from "../../../RecruiterProfileTopJobs/RecruiterProfileTopJobs";
+import "./DesktopRecruiterTopJobs.scss"
+
+function DesktopRecruiterTopJobs({recruiterJobs}) {
+    const [recruiterTopJobs, setRecruiterTopJobs] = useState([])
+
+    useEffect(() => {
+        setRecruiterTopJobs(getTwoHighestAppliedToJobs(recruiterJobs))
+    },[recruiterTopJobs])
+
+    return (
+        <div className="desktopRecruiterTopJobs">
+            <span className="desktopRecruiterTopJobs_header">Top Jobs</span>
+        {recruiterTopJobs.length > 0 ? (
+      <section className="desktopRecruiterTopJobs_topJobs">
+        {recruiterTopJobs.map((jobObj) => (
+          <RecruiterProfileTopJobs key={uuidv4()} jobObj={jobObj} />
+        ))}
+      </section>
+    ) :
+    <span>None of Your Current Jobs Have Applicants</span>}
+        </div>
+    );
+}
+
+export default DesktopRecruiterTopJobs;
