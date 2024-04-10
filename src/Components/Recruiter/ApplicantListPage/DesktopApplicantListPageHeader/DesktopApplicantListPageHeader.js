9493e4f0ffa0e8703c08ccfe0b916140cbd21ef7
DestinyJoyner <destinyjoyner@pursuit.org>
2024-04-10 17:05:35 -0400

    applicant list view page completed

--- src/Components/Recruiter/ApplicantListPage/DesktopApplicantListPageHeader/DesktopApplicantListPageHeader.js
+++ src/Components/Recruiter/ApplicantListPage/DesktopApplicantListPageHeader/DesktopApplicantListPageHeader.js
@@ -2,8 +2,9 @@ import "./DesktopApplicantListPageHeader.scss"
 
 function DesktopApplicantListPageHeader(props) {
     return (
-        <div className="desktopApplicantListPageHeader">
-            Header
+        <div className="desktopApplicantListPageHeader flex-column">
+            <h2>Browse Current Applicants</h2>
+            <span> Connecting Your Organization with Entry-Level Software Engineers Ready to Launch Their Tech Careers</span>
         </div>
     );
 }
