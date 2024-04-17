9eb5d63187f625f02d1bab35f0f279b77877174e
DestinyJoyner <destinyjoyner@pursuit.org>
2024-04-17 15:27:58 -0400

    data file for davbar dropdown objects and component updated

--- src/Components/App/NavBar/DesktopNav/DesktopNavBarLinks/DesktopNavBarLinks.js
+++ src/Components/App/NavBar/DesktopNav/DesktopNavBarLinks/DesktopNavBarLinks.js
@@ -3,63 +3,14 @@ import { Link, useLocation } from "react-router-dom";
 import { useNavProvider } from "../../../../../Providers/NavProvider";
 import { useContextProvider } from "../../../../../Providers/Provider";
 import NavBarLinkDropDown from "../../NavBarLinkDropDown/NavBarLinkDropDown";
-
-import { ImProfile } from "react-icons/im";
-import { FaEdit, FaUserTie } from "react-icons/fa";
-import { AiFillFileAdd } from "react-icons/ai";
-import { MdDeleteForever } from "react-icons/md";
 import "./DesktopNavBarLinks.scss";
 
 function DesktopNavBarLinks({ path, label, clickfunction }) {
   const { navbarClick } = useNavProvider();
   const { isRecruiterAcc } = useContextProvider();
-  const location = useLocation()
+  const location = useLocation();
   const [showNavDropdown, setShowNavDropdown] = useState(false);
   const [menuSelected, setMenuSelected] = useState(false);
-
-  const navBarLinkDropdownValues = {
-    "/user": [
-      { value:<div className="navBarLinkDropDown_links">
-      <ImProfile />
-      <span>View Profile</span>
-      <p>Explore your personal information, resume, job applications, and skills at a glance.</p>
-      </div>, route: "/user" },
-      { value: <div className="navBarLinkDropDown_links">
-      <FaEdit />
-      <span>Edit Profile</span>
-      <p>Edit your education, skills, projects, personal info, and resume to keep your profile up to date.</p>
-      </div>, route: "/user/edit" },
-      { value: <div className="navBarLinkDropDown_links">
-      <MdDeleteForever />
-      <span>Delete Profile</span>
-      <p>Permanently delete your profile and remove all current job applications from consideration.</p>
-      </div>, route: "/" },
-    ],
-    "/recruiter": [
-      { value: <div className="navBarLinkDropDown_links">
-      <AiFillFileAdd />
-      <span>Post New Job</span>
-      <p>Share new opportunities for entry-level software engineers and connect with talent on our platform.</p>
-      </div>, route: "/jobs/new" },
-      { value: <div className="navBarLinkDropDown_links">
-      <FaUserTie />
-      <span>View Profile</span>
-      <p>Browse and oversee your job postings, company information, and evaluate applicants for your listings.</p>
-      </div>
-        , route: "/recruiter" },
-      // { value: <div className="navBarLinkDropDown_links">
-      // <FaEdit />
-      // <span>Edit Profile</span>
-      // <p>Update your organization information, and effortlessly manage your posted job listings.</p>
-      // </div>, route: "/recruiter" },
-      { value: <div className="navBarLinkDropDown_links">
-      <MdDeleteForever />
-      <span>Delete Profile</span>
-      <p>Permanently delete your profile and remove all current job postings from inIT.</p>
-      </div>, route: "/" },
-    ],
-  };
-
   const timeoutRef = useRef(null);
 
   const handleLinkMouseEnter = () => {
@@ -86,8 +37,9 @@ function DesktopNavBarLinks({ path, label, clickfunction }) {
   };
 
   useEffect(() => {
-    setShowNavDropdown(false)
-  },[location.pathname])
+    setShowNavDropdown(false);
+  }, [location.pathname]);
+
   return (
     <div
       className={
@@ -105,25 +57,17 @@ function DesktopNavBarLinks({ path, label, clickfunction }) {
         onMouseEnter={handleLinkMouseEnter}
         onMouseLeave={handleLinkMouseLeave}
       >
-        <span>
-          {label}
-        </span>
+        <span>{label}</span>
       </Link>
 
       {showNavDropdown && (
         <NavBarLinkDropDown
-          dropdownLinks={navBarLinkDropdownValues[path]}
+          path={path}
           setShowNavDropdown={setShowNavDropdown}
           handleMenuMouseEnter={handleMenuMouseEnter}
           handleMenuMouseLeave={handleMenuMouseLeave}
         />
       )}
-     {/* <NavBarLinkDropDown
-          dropdownLinks={navBarLinkDropdownValues[path]}
-          setShowNavDropdown={setShowNavDropdown}
-          handleMenuMouseEnter={handleMenuMouseEnter}
-          handleMenuMouseLeave={handleMenuMouseLeave}
-        /> */}
     </div>
   );
 }
--- src/Components/App/NavBar/NavBarLinkDropDown/NavBarLinkDropDown.js
+++ src/Components/App/NavBar/NavBarLinkDropDown/NavBarLinkDropDown.js
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import navBarLinkDropdownValues from "../../Data/DesktopNavBarLinkContentObj";
 import "./NavBarLinkDropDown.scss";
 
-function NavBarLinkDropDown({ dropdownLinks, setShowNavDropdown, handleMenuMouseEnter, handleMenuMouseLeave}) {
+function NavBarLinkDropDown({
+  path,
+  handleMenuMouseEnter,
+  handleMenuMouseLeave,
+}) {
+  const dropdownLinks = navBarLinkDropdownValues[path];
   return (
     dropdownLinks && (
-      <div className="navBarLinkDropDown init-card"
-      // onMouseLeave={() =>
-      //   setShowNavDropdown(false)}
-      onMouseEnter={handleMenuMouseEnter}
-          onMouseLeave={handleMenuMouseLeave}
-
-
+      <div
+        className="navBarLinkDropDown init-card"
+        onMouseEnter={handleMenuMouseEnter}
+        onMouseLeave={handleMenuMouseLeave}
       >
         {dropdownLinks.map(({ value, route }) => (
-          <Link 
-          to={route} 
-          key={uuidv4()}>
+          <Link to={route} key={uuidv4()}>
             {value}
           </Link>
         ))}
