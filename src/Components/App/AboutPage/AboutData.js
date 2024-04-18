aa3a689a6ad82390d78aac0a668b608785f7cdc6
DestinyJoyner <destinyjoyner@pursuit.org>
2024-04-18 17:32:58 -0400

    desktop about page updated finished with styling

--- src/Components/App/AboutPage/AboutData.js
+++ src/Components/App/AboutPage/AboutData.js
@@ -11,7 +11,7 @@ const team = {
     bio:
     <>Born and raised in New York City, I've always had a flare for solving math and logic puzzles.<br/> She is intrigued by the vast opportunities that virtual augmentation enables users to experience. <br/> By continuously expanding her programming knowledge, Destiny aims to build immersive worlds inside of the virtual universe.
     </>,
-    bio2: <>Born and raised in New York city, I am a software engineer specializing in full stack web development. I am an ethusiastic programmer intrigued by all the things made possible through connecting people all over the world with technology. <br/> Coming from a non-traditional training background, it was a hassle finding entry level tech opportunities.I built inIT to expedite the job search process by directly connecting recruiters looking to fill positions with entry level software developers looking to land their first role in the tech industry.
+    bio2: <>Born and raised in New York city, I am an aspiring mobile application developer with extensive training in full stack web development. I am an ethusiastic programmer intrigued by all the things made possible through connecting people all over the world with technology especially if this connection is made through the tap of a finger. <br/><br/> Coming from a non-traditional training background, it was a hassle finding entry level tech opportunities. I built inIT to expedite the job search process by directly connecting recruiters looking to fill positions, with entry level software developers looking to land their first role in the tech industry. <br /><br/> Through my drive for continuous learning, and keeping up to date with modern tech advancements I hope to contribute in strengthening the connection between people and the applications they use to improve the quality of their everyday lives. 
     </> ,
     links: [
       "https://github.com/DestinyJoyner",
@@ -24,7 +24,9 @@ const team = {
     id: 2,
     name: "About the Application",
     img: group,
-    bio:'inIT is the solution born from the frustration shared by countless entry-level developers navigating the labyrinthine landscape of job search portals and social media platforms cluttered with misleading opportunities. Too often, these platforms label positions as "entry-level," only to reveal hidden prerequisites for prior work experience. For budding developers fresh out of college or boot camps, armed solely with training and perhaps a single portfolio project, this revelation can be disheartening, casting a shadow over their job search journey. Recognizing this challenge, inIT emerges as a beacon of clarity and opportunity, dedicated to connecting recent graduates with genuine entry-level software engineering positions tailored to their technical proficiency. Unlike many platforms, every opportunity hosted on inIT is meticulously curated to require little to no prior work experience. We believe that to flourish in the tech industry, one must first find fertile ground to grow, and inIT aims to be that fertile ground, nurturing and propelling your tech career forward.',
+    bio:<>inIT is the solution born from the frustration shared by countless entry-level developers navigating the labyrinthine landscape of job search portals and social media platforms cluttered with misleading opportunities. 
+    <br /><br/>Too often, these platforms label positions as "entry-level," only to reveal hidden prerequisites for prior work experience. For budding developers fresh out of college or boot camps, armed solely with training and perhaps a single portfolio project, this revelation can be disheartening, casting a shadow over their job search journey. <br /><br/>Recognizing this challenge, inIT emerges as a beacon of clarity and opportunity, dedicated to connecting recent graduates with genuine entry-level software engineering positions tailored to their technical proficiency. 
+    <br /><br/>Unlike many platforms, every opportunity hosted on inIT is meticulously curated to require little to no prior work experience. We believe that to flourish in the tech industry, one must first find fertile ground to grow, and inIT aims to be that fertile ground, nurturing and propelling your tech career forward.</>,
     bio2: <> inIt is a job search web engine  designed for recent college and boot camp graduates looking for their first role in tech. Some of inIT's features include a fully responsive web design, extensive search filtering options and a UX/UI that supports both recruiter and applicant roles</>,
     links: [1,2,3,90,91,0],
   },
@@ -50,6 +52,12 @@ const desktopAboutInitTech = {
     header:"Back End Development",
     subheader: "Powering inIT's functionality with a custom Express/Node.js API, PostgreSQL/SQL for robust database management, and fortified security through BCrypt and JWT authentication. Building a seamless foundation for data management and user protection.",
     icons: [91,90,2,]
+  },
+  ff: {
+    header: "Future Features",
+    subheader: <>
+    <li>inIT is in the process of being developed into a mobile application for expanded accessibility</li>
+    <li>inIT will be integrated with Google Maps API in order to accurately display available opportunities locally for its users</li><li>inIT will provide valuable resources to developers to keep their skills sharp while they seek employment</li></>
   }
 }
 
--- src/Components/App/AboutPage/DesktopAboutPage/DesktopAboutPage.js
+++ src/Components/App/AboutPage/DesktopAboutPage/DesktopAboutPage.js
@@ -1,62 +1,67 @@
-import React from 'react';
-import SkillsComponent from '../../../Job/SkillsComponent';
-import { team, desktopAboutInitTech } from '../AboutData';
-import logo from "../../../../Assets/LOGO.png"
-import "./DesktopAboutPage.scss"
-
+import React from "react";
+import SkillsComponent from "../../../Job/SkillsComponent";
+import ContactLinks from "../../../ContactLinks/ContactLinks";
+import { team, desktopAboutInitTech } from "../AboutData";
+import logo from "../../../../Assets/LOGO.png";
+import "./DesktopAboutPage.scss";
 
 function DesktopAboutPage(props) {
-    return (
-        <div className='desktopAboutPage'>
-            
-            <section className='desktopAboutPage_header init-card '>
-                <img src={logo} alt="init-logo" />
-                <span>Your first tech opportunity awaits.... </span>
-            </section>
-            
-            <section className='desktopAboutPage_init'>
-                <div className='desktopAboutPage_init_left'>
-                    <h2>About inIT</h2>
-                    <p>
-                    {team[2]["bio"]}
-                    </p>
-                </div>
+  return (
+    <div className="desktopAboutPage init-card">
+      <section className="desktopAboutPage_init">
+        <section className="desktopAboutPage_init_header">
+          <img src={logo} alt="init-logo" />
+          <span>Your first tech opportunity awaits.... </span>
+        </section>
 
-<hr/>
-                <div className='desktopAboutPage_init_right'>
-                    <div className='desktopAboutPage_init_right_content'>
-                        <h3>{desktopAboutInitTech["fe"].header}</h3>
-                        <p>{desktopAboutInitTech["fe"].subheader}</p>
-                        <SkillsComponent 
-          justList={true} 
-          skillsArr={desktopAboutInitTech["fe"].icons} 
-          />
-                    </div>
-
-                    <div className='desktopAboutPage_init_right_content'>
-                        <h3>{desktopAboutInitTech["be"].header}</h3>
-                        <p>{desktopAboutInitTech["be"].subheader}</p>
-                        <SkillsComponent 
-          justList={true} 
-          skillsArr={desktopAboutInitTech["be"].icons} 
-          />
-                    </div>
-
-                    <div className='desktopAboutPage_init_right_content'>
-                        <h3>{desktopAboutInitTech["fe"].header}</h3>
-                        <p>{desktopAboutInitTech["fe"].subheader}</p>
-                        
-                    </div>
-                </div>
-            </section>
-
-            <section className='desktopAboutPage_developer'>
-
-            </section>
-
-            
+        <div className="desktopAboutPage_init_left">
+          <p>{team[2]["bio"]}</p>
         </div>
-    );
+
+        <hr />
+
+        <div className="desktopAboutPage_init_right">
+          <div className="desktopAboutPage_init_right_content">
+            <h3>{desktopAboutInitTech["fe"].header}</h3>
+            <p>{desktopAboutInitTech["fe"].subheader}</p>
+            <SkillsComponent
+              justList={true}
+              skillsArr={desktopAboutInitTech["fe"].icons}
+            />
+          </div>
+
+          <div className="desktopAboutPage_init_right_content">
+            <h3>{desktopAboutInitTech["be"].header}</h3>
+            <p>{desktopAboutInitTech["be"].subheader}</p>
+            <SkillsComponent
+              justList={true}
+              skillsArr={desktopAboutInitTech["be"].icons}
+            />
+          </div>
+
+          <div className="desktopAboutPage_init_right_content">
+            <h3>{desktopAboutInitTech["ff"].header}</h3>
+            <p>{desktopAboutInitTech["ff"].subheader}</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="desktopAboutPage_developer">
+        <div className="desktopAboutPage_developer_left">
+          <h2>Meet the Developer</h2>
+          <p>{team[1]["bio2"]}</p>
+          <ContactLinks pursuit={true} />
+        </div>
+
+        <hr />
+
+        <div className="desktopAboutPage_developer_right">
+          <h2>Destiny Joyner</h2>
+          <img src={team[1]["img"]} alt="init-developer" />
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default DesktopAboutPage;
